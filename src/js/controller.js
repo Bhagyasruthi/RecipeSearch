1105478416e32e4061a7d41eb3836b2dba663617
Bhagyasruthi <56640231+Bhagyasruthi@users.noreply.github.com>
2021-01-05 22:31:13 +0000

    commit all

---
+++ src/js/controller.js
@@ -0,0 +1,48 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime';
+import { async } from 'regenerator-runtime';
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+    // Loading Recipe
+    await model.loadRecipe(id);
+
+    //Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+    // Render Results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
