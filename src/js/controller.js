8f4c3cc9bc9b620eccb031df9a62862a72b206cb
Bhagyasruthi <56640231+Bhagyasruthi@users.noreply.github.com>
2021-01-06 22:02:18 +0000

    Pagination
    rendering of items avilable per seach item and divided them 10 per  page.
    rounding the vlaue using Math.ceil
    
    Co-Authored-By: sruthi <318579+sruthi@users.noreply.github.com>

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime';
@@ -37,10 +38,14 @@ const controlSearchResults = async function () {
 
     // 2) Load search results
     await model.loadSearchResults(query);
-    // Render Results
 
+    // 3)Render Results
     //resultsView.render(model.state.search.results);
+
     resultsView.render(model.getSearchResultsPage());
+
+    // 4)Render inital pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
